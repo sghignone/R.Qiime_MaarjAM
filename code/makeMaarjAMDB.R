1cc93a03f1c5d09c96bed1dcc7986c2c05be9345
Marco Chiapello <mc983.cam@gmail.com>
2017-04-07 12:22:52 +0100

    Added code to check for datasets overlap

--- code/makeMaarjAMDB.R
+++ code/makeMaarjAMDB.R
@@ -73,18 +73,50 @@ write.table(all.ordered_taxo, "results/all_ordered_taxo.txt", sep = "\t",
 ##PART TWO##
 #PREPARATION OD THE FASTA FILE
 paraglom.seq <- readBStringSet("data/raw/sequence_export_Paraglomeromycetes.txt","fasta") #433
-names(paraglom.seq)<-gsub("gb\\|", "", names(paraglom.seq))
+names(paraglom.seq) <- gsub("gb\\|", "", names(paraglom.seq))
 archaeo.seq <- readBStringSet("data/raw/sequence_export_Archaeosporomycetes.txt", "fasta") #778
-names(archaeo.seq)<-gsub("gb\\|", "", names(archaeo.seq))
+names(archaeo.seq) <- gsub("gb\\|", "", names(archaeo.seq))
 glomerom.seq <- readBStringSet("data/raw/sequence_export_Glomeromycetes_sanger.txt", "fasta") #23631 (454: 16985 / Sanger: 6646)
-names(glomerom.seq)<-gsub("gb\\|", "", names(glomerom.seq))
+names(glomerom.seq) <- gsub("gb\\|", "", names(glomerom.seq))
 #append
 #append(x, values, after=length(x)), x and values are XStringSet objects
 #all.seq <- append(paraglom.seq,archaeo.seq,after=length(paraglom.seq)) #For testing
 all.seq <- append(paraglom.seq, c(archaeo.seq,glomerom.seq), after=length(paraglom.seq)) #For production
+#filter out  YYY00000 - 788 values)
+all.seq <- all.seq[names(all.seq) != "YYY00000"]
 #order
 all.ordered.seq <- all.seq[order(as.character((names(all.seq))))]
-#filter out  YYY00000 - 788 values)
-all.ordered.good.seq <- all.ordered.seq[names(all.ordered.seq) != "YYY00000"]
 #save (da aggiungere skip su YYY00000 - 788 values)
 writeXStringSet(all.ordered.good.seq, "results/maarjAM.2017.fasta", format="fasta")
+
+
+
+#####################
+names(all.ordered.seq)[which(!(names(all.ordered.seq) %in% all.ordered_taxo[,1]))]
+all.ordered_taxo[,1][which(!(all.ordered_taxo[,1] %in% names(all.ordered.seq)))]
+
+# Load limma library
+library(limma)
+
+# Create protein universe
+universe <- unique(c(as.character(names(all.ordered.seq)),
+					 as.character(all.ordered_taxo[,1]))) 
+
+# Create data.frame for venn diagram
+tmp <- data.frame(seq = rep(0, length(universe)), taxo = rep(0, length(universe)))
+rownames(tmp) <- universe
+for (i in 1:length(universe)){
+	if (universe[i] %in% names(all.ordered.seq)) {
+		tmp[i, 1] <- 1
+	}
+	if (universe[i] %in% as.character(all.ordered_taxo[, 1])){
+		tmp[i, 2] <- 1
+	}
+}
+
+# Venn Diagram
+vennDiagram(tmp)
+
+# Double check the absense of the 10 sequences
+tmp2 <- rownames(tmp[which(tmp$seq ==1 & tmp$taxo != 1), ])
+all.ordered_taxo[which(all.ordered_taxo$V1 %in% tmp2), ]
